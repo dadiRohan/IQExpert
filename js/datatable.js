61f8ba0923bbb5552beb42ddad2d503e4e18d3dc
Rohan Sable <39899398+dadiRohan@users.noreply.github.com>
2025-06-18 16:17:15 +0530

    init

---
+++ js/datatable.js
@@ -0,0 +1,36 @@
+/*$(document).ready(function(){
+    $('#example').DataTable();
+});*/
+
+$(document).ready(function() {
+    // Setup - add a text input to each footer cell
+    $('#example tfoot th').each( function () {
+        var title = $(this).text();
+        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    } );
+ 
+    // DataTable
+    var table = $('#example').DataTable();
+ 
+    // Apply the search
+    table.columns().every( function () {
+        var that = this;
+ 
+        $( 'input', this.footer() ).on( 'keyup change', function () {
+            if ( that.search() !== this.value ) {
+                that
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );
+} );
+
+/*$(document).ready(function(){
+   var table = $('#example').DataTable({
+      dom: 'Alfrtip',
+      alphabetSearch: {
+         column: 0
+      }      
+   });
+});*/
